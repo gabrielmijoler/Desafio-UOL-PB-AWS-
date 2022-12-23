23b39cc89a6fcdd5690f19b699469ebbff3ebb37
gabrielmijoler <gabrielmijoler@hotmail.com>
2022-12-23 15:38:43 -0300

    Finalizando html e css
    Começou js

---
+++ Account/account.js
@@ -0,0 +1,38 @@
+// interface {
+//     next: boolean
+// }
+
+// function verificar(next: boolean){
+//     let next = document.getElementById("nextbt")
+//     let check = document.getElementById("nextbt")
+//     if (check){
+//         return next
+//     } else{
+//         next=false
+//     }
+// }
+
+function envia(){
+    var valores = document.getElementById("operatefinancial");
+    var formValid = false;
+    
+    var i = 0;
+    while (!formValid && i < valores.length) {
+        if (valores[i].checked) formValid = true;
+        i++;        
+    }
+}
+
+function validateForm() {
+    var radios = document.getElementsByName("operatefinancial");
+    var formValid = false;
+
+    var i = 0;
+    while (!formValid && i < radios.length) {
+        if (radios[i].checked) formValid = true;
+        i++;        
+    }
+
+    if (!formValid) alert("Selecionar um botao");
+    return formValid;
+}​
