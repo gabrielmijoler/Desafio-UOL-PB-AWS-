ddb8fa1a37d5ccde4c16a201fd392731f9b58369
gabrielmijoler <gabrielmijoler@hotmail.com>
2022-12-24 13:25:06 -0300

    funções js

--- Account/account.js
+++ Account/account.js
@@ -12,27 +12,27 @@
 //     }
 // }
 
-function envia(){
-    var valores = document.getElementById("operatefinancial");
-    var formValid = false;
+// function envia(){
+//     var valores = document.getElementById("operatefinancial");
+//     var formValid = false;
     
-    var i = 0;
-    while (!formValid && i < valores.length) {
-        if (valores[i].checked) formValid = true;
-        i++;        
-    }
-}
+//     var i = 0;
+//     while (!formValid && i < valores.length) {
+//         if (valores[i].checked) formValid = true;
+//         i++;        
+//     }
+// }
 
-function validateForm() {
-    var radios = document.getElementsByName("operatefinancial");
-    var formValid = false;
+// function validateForm() {
+//     var radios = document.getElementsByName("operatefinancial");
+//     var formValid = false;
 
-    var i = 0;
-    while (!formValid && i < radios.length) {
-        if (radios[i].checked) formValid = true;
-        i++;        
-    }
+//     var i = 0;
+//     while (!formValid && i < radios.length) {
+//         if (radios[i].checked) formValid = true;
+//         i++;        
+//     }
 
-    if (!formValid) alert("Selecionar um botao");
-    return formValid;
-}​
+//     if (!formValid) alert("Selecionar um botao");
+//     return formValid;
+// }
