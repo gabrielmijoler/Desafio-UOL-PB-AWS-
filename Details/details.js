a7792dfee06727ccd8d281eaf2d1658f45b58591
gabrielmijoler <gabrielmijoler@hotmail.com>
2022-12-26 15:56:56 -0300

    Revert "textarea"
    This reverts commit 73bc2d9d24e8152f2323f61698342b71973e54fe.

--- Details/details.js
+++ Details/details.js
@@ -1,23 +1,13 @@
-// myTextArea = document.querySelector('textarea');
+myTextArea = document.querySelector('textarea');
 
 
-// myTextArea.addEventListener('input', function textArea(){
-//         let digitando = myTextArea.value.lenght;
+myTextArea.addEventListener('input', function textArea(){
+    let digitando = myTextArea.value.lenght;
 
-//         if(digitando > 1){
-//             console.error("erro")
-//         } else{
-//             location.href='/Login/index.html'
-//         }
-//     }
-// )
-function validate(){
-        if (document.getElementById("comments").value == ""){
-        alert("Preencher o campo");
-        document.getElementById("comments").focus();
-        }
-        else {
-        // alert(document.getElementById("comments").value);
+    if(digitando > 1){
+        console.error("erro")
+    } else{
         location.href='/Login/index.html'
-        }
+    }
 }
+)
