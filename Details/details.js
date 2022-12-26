73bc2d9d24e8152f2323f61698342b71973e54fe
gabrielmijoler <gabrielmijoler@hotmail.com>
2022-12-26 13:08:20 -0300

    textarea

--- Details/details.js
+++ Details/details.js
@@ -1,13 +1,23 @@
-myTextArea = document.querySelector('textarea');
+// myTextArea = document.querySelector('textarea');
 
 
-myTextArea.addEventListener('input', function textArea(){
-    let digitando = myTextArea.value.lenght;
+// myTextArea.addEventListener('input', function textArea(){
+//         let digitando = myTextArea.value.lenght;
 
-    if(digitando > 1){
-        console.error("erro")
-    } else{
+//         if(digitando > 1){
+//             console.error("erro")
+//         } else{
+//             location.href='/Login/index.html'
+//         }
+//     }
+// )
+function validate(){
+        if (document.getElementById("comments").value == ""){
+        alert("Preencher o campo");
+        document.getElementById("comments").focus();
+        }
+        else {
+        // alert(document.getElementById("comments").value);
         location.href='/Login/index.html'
-    }
+        }
 }
-)
