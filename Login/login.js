24392d866afe849485d90bf194aca9b2cdd927cb
gabrielmijoler <gabrielmijoler@hotmail.com>
2022-12-26 13:24:52 -0300

    reajustando

--- Login/login.js
+++ Login/login.js
@@ -1,71 +1,71 @@
 
 
-const form = document.getElementById("form");
-const username = document.getElementById("name");
-const email = document.getElementById("email");
-const age = document.getElementById("age");
+// const form = document.getElementById("form");
+// const username = document.getElementById("name");
+// const email = document.getElementById("email");
+// const age = document.getElementById("age");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
-    checkInput();
-})
+//     checkInput();
+// })
 
-function checkInput(){
-    const nameValue = username.value;
-    const emailValue = email.value;
-    const ageValue = age.value;
+// function checkInput(){
+//     const nameValue = username.value;
+//     const emailValue = email.value;
+//     const ageValue = age.value;
 
-    if(nameValue === ""){
-        setErrorFor(username, "O nome de usuário é obrigatorio")
-        console.log(setErrorFor)
-    }else {
-        setSuccessFor(username);
-    }
+//     if(nameValue === ""){
+//         setErrorFor(username, "O nome de usuário é obrigatorio")
+//         console.log(setErrorFor)
+//     }else {
+//         setSuccessFor(username);
+//     }
 
-    if (emailValue === "") {
-        setErrorFor(email, "O email é obrigatório.");
-    } else {
-        setSuccessFor(email);
-    }
+//     if (emailValue === "") {
+//         setErrorFor(email, "O email é obrigatório.");
+//     } else {
+//         setSuccessFor(email);
+//     }
 
-    if (ageValue === "") {
-        setErrorFor(age, "A idade é obrigatorio.");
-    } else if(typeof ageValue === 'number'){
-        setErrorFor(age, "A idade não é numero.");
-    }else {
-        setSuccessFor(age);
-    }
+//     if (ageValue === "") {
+//         setErrorFor(age, "A idade é obrigatorio.");
+//     } else if(typeof ageValue === 'number'){
+//         setErrorFor(age, "A idade não é numero.");
+//     }else {
+//         setSuccessFor(age);
+//     }
 
-    const formControls = form.querySelectorAll(".form-control");
+//     const formControls = form.querySelectorAll(".form-control");
 
-    const formIsValid = [...formControls].every((formControl) => {
-    return formControl.className === "form-control success";
-    });
+//     const formIsValid = [...formControls].every((formControl) => {
+//     return formControl.className === "form-control success";
+//     });
 
-    if (formIsValid) {
-    console.log("O formulário está 100% válido!");
-    }
-}
-
-
+//     if (formIsValid) {
+//     console.log("O formulário está 100% válido!");
+//     }
+// }
 
 
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+
+
+// function setErrorFor(input, message) {
+//     const formControl = input.parentElement;
+//     const small = formControl.querySelector("small");
   
-    // Adiciona a mensagem de erro
-    small.innerText = message;
+//     // Adiciona a mensagem de erro
+//     small.innerText = message;
   
-    // Adiciona a classe de erro
-    formControl.className = "form-control error";
-  }
+//     // Adiciona a classe de erro
+//     formControl.className = "form-control error";
+//   }
   
-  function setSuccessFor(input) {
-    const formControl = input.parentElement;
+//   function setSuccessFor(input) {
+//     const formControl = input.parentElement;
   
-    // Adicionar a classe de sucesso
-    formControl.className = "form-control success";
-  }
+//     // Adicionar a classe de sucesso
+//     formControl.className = "form-control success";
+//   }
   
